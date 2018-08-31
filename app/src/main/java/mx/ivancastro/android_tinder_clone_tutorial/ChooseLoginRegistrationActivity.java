4da0863c2bba2cdd784449a5ba40f7a1c4187a48
Ivan Jimenez <ivan.drt.01.03@gmail.com>
2018-08-31 17:21:16 -0600

    commit from android studio

---
+++ app/src/main/java/mx/ivancastro/android_tinder_clone_tutorial/ChooseLoginRegistrationActivity.java
@@ -0,0 +1,41 @@
+package mx.ivancastro.android_tinder_clone_tutorial;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseLoginRegistrationActivity extends AppCompatActivity {
+    private Button mLogin;
+    private Button mRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login_registration);
+
+        mLogin    = findViewById(R.id.login);
+        mRegister = findViewById(R.id.register);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseLoginRegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseLoginRegistrationActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
